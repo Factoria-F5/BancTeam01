63e21ffc007da4fd7064ce76fecf034f1072a81f
EL <joselezama.emilio@gmail.com>
2019-04-17 17:46:12 +0200

    Holi :)

---
+++ database/factories/BeneficiaryFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Beneficiary::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/OfferFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Offer::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
