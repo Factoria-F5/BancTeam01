9bc0df74642ee3f8ede4da3e0a0fcdceba950b20
Edu <nikopol.edu@gmail.com>
2019-05-30 16:39:53 +0200

    Registro y Login
    Añadido registro de usuarios, login y recuperación de contraseña mediante correo (mailtrap.io)

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 
 Route::resource('offer', 'OfferController');
 Route::resource('beneficiary', 'BeneficiaryController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
