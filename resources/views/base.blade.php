dd7f734770606711c7c8b677fdc170711f9769cd
El_Piola <leandrovejar@gmail.com>
2019-06-13 12:59:21 +0200

    ahora sí funciona beneficiary :)

---
+++ resources/views/base.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Backend F5</title>
+    <link href="{{ asset('css/app.css' )}}" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="logo">
+        <span><img src="http://www.amigaett.org/wp-content/themes/amiga_ett/lib/img/amiga_ett_social2.png" class="float-left" alt="logo"></span>
+        </div>
+        
+        @yield('main')
+        
+        
+    </div>
+    <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+    
+</body>
+</html>
